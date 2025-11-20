124b37209178b26aa008bb35f99be44f9cce3716
GalateaSec <raustinogameplays1724@gmail.com>
2025-11-20 02:11:43 -0600

    Added and applied font

--- zenia-app/src/main.jsx
+++ zenia-app/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
 // Para medir Web Vitals (INP, LCP, CLS):
 // 1. Instalar: npm install web-vitals
